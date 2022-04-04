b5a29877c175c5ef38eba2047d7c75cbc021b9ab
akkago <riabov.s@outlook.com>
2022-04-04 23:16:30 +0300

    test api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import _ from 'lodash';
+
+function CharacterCard(props) {
+  const { item } = props;
+
+  return (
+    <div className='character-card' key={item.id}>
+      <img src={item.image} className="character-card__photo" alt="photo" width={200} height={200} />
+      <div className="character-card__content">
+        <div className="character-card__section" >{item.name}</div>
+        <div className="character-card__section" >{`${item.status} - ${item.species}`}</div>
+        <div className="character-card__section" >{`gender: ${item.gender}`}</div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const [names, setNames] = useState();
+
+  const onClick = async () => {
+    let url = 'https://rickandmortyapi.com/api/character';
+    let response = await fetch(url);
+    let commits = await response.json();
+    setNames(commits.results);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type={'text'}></input>
+      <button onClick={onClick}>поиск</button>
+      <div className='content-container'>
+        {_.map(names, (item) => <CharacterCard item={item} />)}
+      </div>
     </div>
   );
 }
